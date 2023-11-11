c6dd42d8612907df543c8caf3484c261c446f391
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-11-11 12:06:24 +0000

    zero constants and computer initialiser

--- src/computer.rs
+++ src/computer.rs
@@ -14,6 +14,10 @@ impl Bit {
 
 pub struct Byte (Bit, Bit, Bit, Bit, Bit, Bit);
 
+impl Byte {
+    pub const ZERO: Self = Self (Bit::ZERO, Bit::ZERO, Bit::ZERO, Bit::ZERO, Bit::ZERO, Bit::ZERO);
+}
+
 pub enum Sign {
     PLUS,
     MINUS,
@@ -30,9 +34,21 @@ impl Sign {
 
 pub struct Word (Sign, Byte, Byte, Byte, Byte, Byte);
 
+impl Word {
+    pub const ZERO: Self = Self (Sign::PLUS, Byte::ZERO, Byte::ZERO, Byte::ZERO, Byte::ZERO, Byte::ZERO);
+}
+
 pub struct Index (Sign, Byte, Byte);
 
+impl Index {
+    pub const ZERO: Self = Self (Sign::PLUS, Byte::ZERO, Byte::ZERO);
+}
+
 pub struct JumpAddress (Byte, Byte);
+
+impl JumpAddress {
+    pub const ZERO: Self = Self (Byte::ZERO, Byte::ZERO);
+}
 
 pub enum ComparisonIndicatorState {
     EQUAL,
@@ -41,6 +57,10 @@ pub enum ComparisonIndicatorState {
 }
 
 pub struct Memory ([Word; 4000]);
+
+impl Memory {
+    pub const ZERO: Self = Self([Word::ZERO; 4000]);
+}
 
 pub struct Computer {
     loaded_instruction: Option<Word>,
@@ -54,6 +74,26 @@ pub struct Computer {
     r_i6: Index,
     r_j: JumpAddress,
     overflow: bool,
-    comparison_indicator: ComparisonIndicatorState,
+    comparison_indicator: Option<ComparisonIndicatorState>,
     memory: Memory,
+}
+
+impl Computer {
+    pub fn new() -> Self {
+        Self {
+            loaded_instruction: None,
+            r_a: Word::ZERO,
+            r_x: Word::ZERO,
+            r_i1: Index::ZERO,
+            r_i2: Index::ZERO,
+            r_i3: Index::ZERO,
+            r_i4: Index::ZERO,
+            r_i5: Index::ZERO,
+            r_i6: Index::ZERO,
+            r_j: JumpAddress::ZERO,
+            overflow: false,
+            comparison_indicator: None,
+            memory: Memory::ZERO,
+        }
+    }
 }
