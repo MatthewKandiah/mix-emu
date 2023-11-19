6c69c4e4704840a4b321fe4089575d4182705533
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-11-19 13:20:00 +0000

    wip memory methods

--- src/memory.rs
+++ src/memory.rs
@@ -6,8 +6,24 @@ pub struct Memory {
     content: [Word; SIZE],
 }
 
+pub enum AccessError{
+    InvalidAddress (i32),
+}
+
+pub enum SetError{
+    InvalidAddress (i32),
+}
+
 impl Memory {
     pub const ZERO: Self = Self {
         content: [Word::ZERO; SIZE],
     };
+
+    pub fn get(&self, address: i32) -> Result<Word, AccessError> {
+        unimplemented!()
+    }
+
+    pub fn set(&mut self, address: i32, value: Word) -> Result<(), SetError> {
+        unimplemented!()
+    }
 }
