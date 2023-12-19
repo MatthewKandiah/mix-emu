f6523b6dca3486d1da5e3a87f0b27fb334217cc3
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-12-19 11:07:50 +0000

    read source file contents as string

--- src/bin/assemble.rs
+++ src/bin/assemble.rs
@@ -1,12 +1,16 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() == 1 {
         panic!("USAGE: pass in paths to MIXAL source files");
     }
+    if args.len() > 2 {
+        panic!("USAGE: only pass in one MIXAL source file");
+    }
 
     let path = &args[1];
-
-    println!("source file: {}", path);
+    let source_contents = fs::read_to_string(path).expect("Failed to read source file");
+    println!("Contents:\n{source_contents}");
 }
