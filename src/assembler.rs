8563d63e799e7a44c29694b9c3443da2d8404db8
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-12-23 11:03:56 +0000

    split line into 2/3 parts

--- src/assembler.rs
+++ src/assembler.rs
@@ -1,22 +1,20 @@
 use crate::data_types::Word;
 
+type LineParts<'a> = (Option<&'a str>, &'a str, &'a str);
+
 pub fn read_source_string_as_instructions(source: String) -> Vec<Word> {
-    let source_lines: Vec<Vec<&str>> = source
+    let source_lines: Vec<LineParts> = source
         .split('\n')
         .map(str::split_whitespace)
         .map(Iterator::collect)
         .filter(|x: &Vec<&str>| !x.is_empty())
-        .filter(|x: &Vec<&str>| {
-            let is_comment = x
-                .first()
-                .expect("Should have already filtered out empty lines")
-                .chars()
-                .next()
-                .expect("Should have stripped out leading whitespace characters")
-                == '*';
-            return !is_comment;
-        })
+        .filter(is_not_comment)
+        .map(parse_line_parts)
         .collect();
+
+    for line in source_lines {
+        println!("{:?}", line);
+    }
 
     // first pass - determine value of each symbol
     // types of symbol - EQU statements, asterisk addresses, local variables dH, dB, dF, ORIG
@@ -30,4 +28,27 @@ pub fn read_source_string_as_instructions(source: String) -> Vec<Word> {
     let mut result: Vec<Word> = vec![];
     result.push(Word::ZERO);
     return result;
+}
+
+fn is_comment(line: &Vec<&str>) -> bool {
+    line.first()
+        .expect("Should have already filtered out empty lines")
+        .chars()
+        .next()
+        .expect("Should have stripped out leading whitespace characters")
+        == '*'
+}
+
+fn is_not_comment(line: &Vec<&str>) -> bool {
+    !is_comment(line)
+}
+
+fn parse_line_parts(line: Vec<&str>) -> LineParts {
+    if line.len() == 2 {
+        return (None, line[0], line[1]);
+    }
+    if line.len() == 3 {
+        return (Some(line[0]), line[1], line[2]);
+    }
+    panic!("Invalid line");
 }
